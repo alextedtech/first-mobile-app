d6ee0d3c54f5548845cae020194949d0bf11d627
alextedtech <alextedtech@gmail.com>
2023-06-10 13:10:33 -0700

    set variables for button and input element

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let addToCarBtn  = document.getElementById("add-button")
+let addButtonEl  = document.getElementById("add-button")
+let inputFieldEl = document.getElementById("input-field")
 
 addToCarBtn.addEventListener("click", function(){
-    console.log("test");
+    console.log(inputFieldEl.value);
     
 })
