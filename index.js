e4610d92cdd9103423406731feebdfb4b374c2aa
alextedtech <alextedtech@gmail.com>
2023-06-15 10:12:05 -0700

    Created link to firebase

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-let addButtonEl  = document.getElementById("add-button")
-let inputFieldEl = document.getElementById("input-field")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings ={
+    databaseURL:"https://realtime-database-1ee63-default-rtdb.firebaseio.com/"}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+let addButtonEl  = document.getElementById("add-button");
+let inputFieldEl = document.getElementById("input-field");
+
 
 addButtonEl.addEventListener("click", function(){
-    inputValue =inputFieldEl.value
+    let inputValue =inputFieldEl.value;
+    push(shoppingListInDB, inputValue)
     //better to define once so you can use it in the rest of the code. 
     console.log(inputValue);
     
